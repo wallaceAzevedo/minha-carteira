695f60d9ca51b6d917e68f308bb20b0fc3dcb9d8
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-05-24 20:40:30 -0300

    Aula Themes Finalizada

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#1DCDCD',
+        secondary: '#FFFFFF',
+        tertiary:'#F5F5F5',
+
+        white:'#000',
+        black:'#FFF',
+        gray:'#BFBFBF',
+
+        success:'#03BB85',
+        info:'#3B5998',
+        warning:'#FF6961',
+
+    },
+};
