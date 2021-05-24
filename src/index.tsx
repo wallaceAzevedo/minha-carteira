dbb27703208cc53de50685b6389bc93538fe0c86
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-05-24 16:54:47 -0300

    segunda aula

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dashboard from './pages/Dashboard';
-
+import App from './app'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
