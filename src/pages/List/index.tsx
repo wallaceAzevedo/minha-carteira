57d0c4c9f910a57ff864605ab69528f1ba6926fc
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-05-25 22:08:16 -0300

    finalizada aula 20

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,20 +1,54 @@
 import React from 'react';
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
+import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import {Container} from './styles';
+import {Container, Content, Filters} from './styles';
 
 const List: React.FC = () => {
-    const options = [
-        {value:'wallace', label:'wallace'},
-        {value:'maria', label:'maria'},
-        {value:'ana', label:'ana'}
+    const months = [
+        {value:'5', label:'Maio'},
+        {value:'6', label:'Junho'},
+        {value:'7', label:'Julho'}
+    ];
+
+    const years = [
+        {value:'2021', label:2021},
+        {value:'2020', label:2020},
+        {value:'2019', label:2019}
     ];
     return (
         <Container>
             <ContentHeader title="Saidas" lineColor="#E44C4E">
-               <SelectInput options={options}/>
+               <SelectInput options={months}/>
+               <SelectInput options={years}/>
            </ContentHeader>
+
+           <Filters>
+              <button 
+              type="button"
+              className=" tag-filter tag-filter-Recorrent"
+              >
+                  Recorrentes
+              </button>
+
+              <button 
+              type="button"
+              className=" tag-filter tag-filter-eventual"
+              >
+                  Eventuais
+              </button>
+           </Filters>
+            <Content>
+               <HistoryFinanceCard
+                  tagColor="#E44C4E"
+                  title="Conta de Luz"
+                  subtitle="27/07/2020"
+                  amount="R$ 130,00"
+               />
+
+            </Content>
+
         </Container>
     );
 }
