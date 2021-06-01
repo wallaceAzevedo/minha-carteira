6d4bd45c059f4559aab193ddc950ab1244c1da68
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-01 18:56:10 -0300

    aula 59
    ate o momento configuramos  todo leyout exceto o menu

--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -15,6 +15,14 @@ export const Container = styled.div`
   border-radius: 7px;
 
   display: flex;
+
+  @media(max-width: 1200px) {
+    display: flex;
+    flex-direction: column;
+
+    width:100%;
+    height: auto;
+  }
 `;
 
 export const SideLeft = styled.div`
@@ -34,6 +42,11 @@ export const LegendContainer = styled.ul`
   height: 175px;
   padding-right: 15px;
 
+  @media(max-width: 1200px) {
+    display: flex;
+    
+    height: auto;
+  }
 
 `;
 
@@ -62,6 +75,17 @@ export const Legend = styled.li<ILegendProps>`
    > span {
        margin-left: 5px;
    }
+
+   @media(max-width: 1200px) {
+      >div{
+        width: 30px;
+        height: 30px;
+
+        font-size: 10px;
+        line-height: 30px;
+        text-align: center;
+      }
+    }
 `;
 
 export const SideRight = styled.div`
--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -38,6 +38,10 @@ export const Header = styled.header`
         margin-bottom: 20px;
         padding-left: 16px;
     }
+
+    @media(max-width: 1200px){
+        flex-direction: column;
+    }
 `;
 
 export const LegendContainer = styled.ul`
@@ -51,7 +55,7 @@ export const Legend = styled.li<ILegendProps>`
     align-items: center;
 
     margin-bottom: 7px;
-    margin-left: 7px;
+    margin-left: 16px;
     
 
    > div {
@@ -70,4 +74,12 @@ export const Legend = styled.li<ILegendProps>`
    > span {
        margin-left: 5px;
    }
+
+   @media(max-width: 1280px){
+        > div {
+            width: 40px;
+            height: 40px;
+        }
+        
+    }
 `;
