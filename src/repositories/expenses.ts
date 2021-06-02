0a3cce3f42b8daba8a6436181d532120b957a617
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-02 07:30:03 -0300

    expenses

--- src/repositories/expenses.ts
+++ src/repositories/expenses.ts
@@ -82,7 +82,16 @@ export default [
   {"description":"Plano de Saúde",        "amount":"300.00","type":"saída","frequency":"recorrente","date":"2020-05-23"},
   {"description":"Compras do mês",        "amount":"540.00","type":"saída","frequency":"recorrente","date":"2020-05-25"},
   {"description":"Parcela do Celular 4/5","amount":"150.99","type":"saída","frequency":"eventual",  "date":"2020-05-26"},
-  {"description":"Parcela do carro 4/445","amount":"861.67","type":"saída","frequency":"eventual",  "date":"2021-03-26"}
+  {"description":"Parcela do carro 4/445","amount":"861.67","type":"saída","frequency":"eventual",  "date":"2021-03-26"},
+  
+  {"description":"Ebook de React Js",     "amount":"85.95", "type":"saída","frequency":"eventual",  "date":"2021-06-13"},
+  {"description":"Energia elétrica",      "amount":"125.55","type":"saída","frequency":"recorrente","date":"2021-06-10"},
+  {"description":"Água",                  "amount":"90.15", "type":"saída","frequency":"recorrente","date":"2021-06-15"},
+  {"description":"Telefone",              "amount":"99.99", "type":"saída","frequency":"recorrente","date":"2021-06-23"},
+  {"description":"Plano de Saúde",        "amount":"300.00","type":"saída","frequency":"recorrente","date":"2021-06-23"},
+  {"description":"Compras do mês",        "amount":"540.00","type":"saída","frequency":"recorrente","date":"2021-06-25"},
+  {"description":"Parcela do Celular 4/5","amount":"150.99","type":"saída","frequency":"eventual",  "date":"2021-06-26"},
+  {"description":"Parcela do carro 4/445","amount":"861.67","type":"saída","frequency":"eventual",  "date":"2021-03-26"},
 
   
 ]
