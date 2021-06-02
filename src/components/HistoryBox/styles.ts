8cda0a71d3635d7e0216ecec5689fe3394ca438a
wallaceAzevedo <wallace_azevedo17@hotmail.com>
2021-06-02 07:20:04 -0300

    Curso finalizado
    Nesta ultima aula adicionamos animações  ao nosso layout

--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,8 +1,22 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 interface ILegendProps {
     color: string
 }
+
+const animate = keyframes`{
+    0% {
+         transform: translateX(-100px);
+         opacity: 0;
+    }
+    50%{
+         opacity: .3;
+    }
+    100%{
+         transform: translateX(0px);
+         opacity: 1;
+    }
+`;
 
 
 export const Container = styled.div`
@@ -19,7 +33,8 @@ export const Container = styled.div`
     padding: 30px 20px;
 
     border-radius: 7px;
-
+    
+    animation: ${animate} .5s;
     
 `;
 
